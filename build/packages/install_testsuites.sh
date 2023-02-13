5b9e05d02b2626ca91a829a2ca331765355bae86
bigchem <47360534+bigchem@users.noreply.github.com>
2023-02-13 13:10:59 +0100

    Update install_testsuites.sh

--- build/packages/install_testsuites.sh
+++ build/packages/install_testsuites.sh
@@ -1,2 +1,2 @@
 mvn install:install-file -Dfile=testsuites-xml-1.0.jar -DpomFile=testsuites-xml-1.0.pom
-mvn install:install-file -Dfile=jna-inchi-linux-aarch64-1.1.jar -DpomFile=jna-inchi-linux-aarch64-1.1.pom
+#mvn install:install-file -Dfile=jna-inchi-linux-aarch64-1.1.jar -DpomFile=jna-inchi-linux-aarch64-1.1.pom
