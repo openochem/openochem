cfec0e59365f4d5f097d55ce2c909371e37bbde0
bigchem <47360534+bigchem@users.noreply.github.com>
2023-02-16 19:05:58 +0100

    Update stop.sh

--- env/stop.sh
+++ env/stop.sh
@@ -9,4 +9,3 @@ echo "singularity instance stop $NAME"
 
 singularity instance stop $NAME || true;
 rm -rf $NAME/tmp >/dev/null 2>/dev/null
-rm -rf ~/.singularity/instances/sing/qsar/bigchem/$NAME >/dev/null 2>/dev/null
