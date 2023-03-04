293e3144310f9d258d0b825a994b9bd574474751
bigchem <47360534+bigchem@users.noreply.github.com>
2023-03-04 20:32:06 +0100

    still debugging delays

--- demo-data/insert_databases.sh
+++ demo-data/insert_databases.sh
@@ -9,6 +9,6 @@ ROOT=
 mysql $ROOT < sqlsetup.sql
 mysql $ROOT metaserver_demo < metaserver_demo.sql 
 mysql $ROOT ochem_demo < ochem_demo.sql 
-mysql $ROOT structures_demo < structures_demo.sql 
-mysql $ROOT fragment_demo < fragment_demo.sql 
 mysql $ROOT ochem_demo -e "drop table flyway_schema_history"
+mysql $ROOT fragment_demo < fragment_demo.sql 
+mysql $ROOT structures_demo < structures_demo.sql 
