8d7bda499b318e1061e61f8df5844c1223353e3b
bigchem <47360534+bigchem@users.noreply.github.com>
2024-04-14 14:52:10 +0200

    Allowing sufficient time for DB to initialise

--- docker/db_entrypoint.sh
+++ docker/db_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 init_db () {
-        sleep 5
+        sleep 120
         chmod +x insert_databases.sh
 	./insert_databases.sh && echo "DB Initialized." && ls /var/lib/mysql
         touch /var/lib/mysql/ochem_initialized
