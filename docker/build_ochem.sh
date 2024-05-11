ca207fac7ce69177ec650840d24e6dfbae465aba
bigchem <47360534+bigchem@users.noreply.github.com>
2024-05-11 17:33:21 +0200

    Update automatic build script for China

--- docker/build_ochem.sh
+++ docker/build_ochem.sh
@@ -1,2 +1,3 @@
-while ! docker build -t ochem-env -f docker/Dockerfile-China . ; do sleep 4 ; done ; echo succeed ;
+sed -i -r "s|#ARG CHINA|ARG CHINA|" docker/Dockerfile-env 
+while ! docker build -t ochem-env -f docker/Dockerfile-env . ; do sleep 4 ; done ; echo succeed ;
 while ! docker-compose -f docker/ochem-demo.yml build ; do sleep 4 ; done ; echo succeed
